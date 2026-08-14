f8a7818803609ccfb56165da12fcc0621b45f058
Icedk12 <tompattonlow@gmail.com>
2026-08-14 17:58:17 +1000

    Optimised the functionality
    Cut down to only 7 lines

--- scripts/components/CharacterComponent.gd
+++ scripts/components/CharacterComponent.gd
@@ -1,15 +1,7 @@
 class_name CharacterComponent extends Component
 
-@export var character : CharacterBody2D ## Mutually exclusive with isCharChild. (Retard proof)
-@export var is_char_child : bool = false ## Tick when the component is the child of a character, it will then automatically assign it to the player internal variable.
+@export var character : CharacterBody2D ## [OPTIONAL]
 
 func _ready() -> void:
-	if not character == null:
-		# If char was selected in @export
-		is_char_child = false
-	
-	if is_char_child:
-		_is_char_child()
-
-func _is_char_child() -> void:
-	character = get_parent() as CharacterBody2D
+	if character == null:
+		character = get_parent() as CharacterBody2D
