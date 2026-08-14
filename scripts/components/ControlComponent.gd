da0705e0aaf4b97eee2411bc3594d0ec1abbf41e
Icedk12 <tompattonlow@gmail.com>
2026-08-15 09:43:01 +1000

    Added paralax and light

--- scripts/components/ControlComponent.gd
+++ scripts/components/ControlComponent.gd
@@ -7,14 +7,21 @@ func set_velocity(_velocity : Vector2) -> void:
 
 func set_y(_y : float) -> void:
 	if character == null: return
-	
 	character.velocity.y = _y
-
 func set_x(_x : float) -> void:
 	if character == null: return
-	
 	character.velocity.x = _x
 	
+func decelerate_x(delta : float) -> void:
+	move_toward(character.velocity.x, 0, delta)
+func decelerate_y(delta : float) -> void:
+	move_toward(character.velocity.y, 0, delta)
+	
+func accelerate_x(delta : float, target_speed : float) -> void:
+	move_toward(character.velocity.x, target_speed, delta)
+func accelerate_y(delta : float, target_speed : float) -> void:
+	move_toward(character.velocity.y, target_speed, delta)
+
 ## Allows custom behaviour when calling move and slide
 func _move_and_slide_callback() -> void:
 	character.move_and_slide()
--- scripts/components/MovementComponent.gd
+++ scripts/components/MovementComponent.gd
@@ -14,12 +14,20 @@ var input_direction : float = 0.0
 func _physics_process(delta: float) -> void:
 	if _is_character_null(): return
 	
+	
 	input_direction = Input.get_axis("move_left", "move_right") # Store direction
 	var x_velocity : float = input_direction * walk_speed # Calculate x velocity
 	
+	#control_component.set_x(move_toward(x_velocity, )) TODO
+	
 	if x_velocity != 0 and character.is_on_floor():
-		sprite_modifier_component._sprite_sin_offset(delta) # Move the character sprite up and down on a sine wave
+		on_walk(delta)
 	elif sprite_modifier_component.sprite.offset.y != 0:
-		sprite_modifier_component.sprite.offset = Vector2.ZERO
-	control_component.set_x(x_velocity)
+		reset_sprite_offset(delta)
+
+func on_walk(delta : float) -> void:
+	sprite_modifier_component._sprite_sin_offset(delta) # Move the character sprite up and down on a sine wave
+
+func reset_sprite_offset(delta : float) -> void:
+	sprite_modifier_component.sprite.offset = Vector2.ZERO
 	
